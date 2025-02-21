8243e3aff7bfc411568f8b2ed0c45c8e9e10395e
Emily McLean <emilymcleandeveloper@gmail.com>
2025-02-21 16:37:44 +1100

    Improve the documentation

--- requeststate/src/commonMain/kotlin/cl/emilym/compose/requeststate/RequestState.kt
+++ requeststate/src/commonMain/kotlin/cl/emilym/compose/requeststate/RequestState.kt
@@ -1,12 +1,34 @@
 package cl.emilym.compose.requeststate
 
+/**
+ * Represents the state of a data transfer operation between the UI and ViewModel.
+ */
 sealed interface RequestState<T> {
 
+    /**
+     * The data transfer has not begun
+     */
     class Initial<T>: RequestState<T>
+
+    /**
+     * The data transfer is in progress
+     */
     class Loading<T>: RequestState<T>
+
+    /**
+     * The data transfer succeeded
+     *
+     * @param value The result of the data transfer, usually the data itself for read operations
+     */
     data class Success<T>(
         val value: T
     ): RequestState<T>
+
+    /**
+     * The data transfer failed
+     *
+     * @param exception The cause of the failure
+     */
     data class Failure<T>(
         val exception: Throwable
     ): RequestState<T>
